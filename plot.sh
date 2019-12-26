3d814884875049d80381f8a46e00720a41a86106
chemars <37469933+chemars@users.noreply.github.com>
2019-12-26 16:20:05 +0800

    Upload

---
+++ plot.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S bash -e
+export GMT_SESSION_NAME=$$	# Set a unique session name
+data=intensity.csv
+
+gmt begin 
+gmt figure CWB_2000 png A+m1c
+gmt basemap -Jm3c -R119/123/21/26 -Ba1 -B+t"Intensity Map"
+gmt coast -Df -W0.3p
+gawk 'BEGIN {FS=","}; {if (NR > 1) print $3, $4, $5}' $data | gmt text -F+f12p,,blue+jMC
+echo 119.15 21.15 "Intensity scale: CWB (2000)" | gmt text -F+f10p,,black+jML
+
+gmt figure CWB png A+m1c
+gmt basemap -Jm3c -R119/123/21/26 -Ba1 -B+t"Intensity Map"
+gmt coast -Df -W0.3p
+gawk 'BEGIN {FS=","}; {if (NR > 1) print $3, $4, $6}' $data | gmt text -F+f12p,,blue+jMC
+echo 119.15 21.15 "Intensity scale: CWB" | gmt text -F+f10p,,black+jML
+
+gmt figure JMA png A+m1c
+gmt basemap -Jm3c -R119/123/21/26 -Ba1 -B+t"Intensity Map"
+gmt coast -Df -W0.3p
+gawk 'BEGIN {FS=","}; {if (NR > 1) print $3, $4, $8}' $data | gmt text -F+f12p,,blue+jMC
+echo 119.15 21.15 "Intensity scale: JMA" | gmt text -F+f10p,,black+jML
+
+gmt figure FJEA png A+m1c
+gmt basemap -Jm3c -R119/123/21/26 -Ba1 -B+t"Intensity Map"
+gmt coast -Df -W0.3p
+gawk 'BEGIN {FS=","}; {if (NR > 1) print $3, $4, $9}' $data | gmt text -F+f12p,,blue+jMC
+echo 119.15 21.15 "Intensity scale: FJEA" | gmt text -F+f10p,,black+jML
+gmt end
