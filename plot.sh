8ca37b3e0959830428e2e16d4ce03ce538cd48ba
chemars <37469933+chemars@users.noreply.github.com>
2019-12-26 16:29:22 +0800

    改變顏色

--- plot.sh
+++ plot.sh
@@ -6,7 +6,8 @@ gmt begin
 gmt figure CWB_2000 png A+m1c
 gmt basemap -Jm3c -R119/123/21/26 -Ba1 -B+t"Intensity Map"
 gmt coast -Df -W0.3p
-gawk 'BEGIN {FS=","}; {if (NR > 1) print $3, $4, $5}' $data | gmt text -F+f12p,,blue+jMC
+gawk 'BEGIN {FS=","}; {if (NR > 1 && $5 <= 4) print $3, $4, $5}' $data | gmt text -F+f12p,,blue+jMC
+gawk 'BEGIN {FS=","}; {if (NR > 1 && $5 >= 5) print $3, $4, $5}' $data | gmt text -F+f12p,,red+jMC
 echo 119.15 21.15 "Intensity scale: CWB (2000)" | gmt text -F+f10p,,black+jML
 
 gmt figure CWB png A+m1c
@@ -24,6 +25,7 @@ echo 119.15 21.15 "Intensity scale: JMA" | gmt text -F+f10p,,black+jML
 gmt figure FJEA png A+m1c
 gmt basemap -Jm3c -R119/123/21/26 -Ba1 -B+t"Intensity Map"
 gmt coast -Df -W0.3p
-gawk 'BEGIN {FS=","}; {if (NR > 1) print $3, $4, $9}' $data | gmt text -F+f12p,,blue+jMC
+gawk 'BEGIN {FS=","}; {if (NR > 1 && $9 <= 5) print $3, $4, $9}' $data | gmt text -F+f12p,,blue+jMC
+gawk 'BEGIN {FS=","}; {if (NR > 1 && $9 >= 6) print $3, $4, $9}' $data | gmt text -F+f12p,,red+jMC
 echo 119.15 21.15 "Intensity scale: FJEA" | gmt text -F+f10p,,black+jML
 gmt end
